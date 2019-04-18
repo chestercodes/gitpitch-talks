8b8c09ede9bfb1107854a839e166c94d2122ddb2
Chester Burbidge <Chester.Burbidge@parmenion.co.uk>
2019-04-18 14:14:47 +0100

    Added stuff

--- full-stack-development/code/syntax.fsx
+++ full-stack-development/code/syntax.fsx
@@ -1,20 +1,20 @@
 // adapted from https://fsharpforfunandprofit.com/posts/fsharp-in-60-seconds/
 let myInt = 5
 let myFloat = 3.14
-let myString = "hello"
+let ``a string value`` = "hello"
 
 // mutability requires mutable keyword and special assignment operator
 let mutable counter = 0
 counter <- counter + 1
 
-let twoToFive = [ 2; 3; 4; 5 ]        // Square brackets create a list.
+let twoToFive = [ 2; 3; 4; 5 ]        // create a list.
 let oneToFive = 1 :: twoToFive        // :: creates list with new 1st element
 let zeroToFive = [ 0; 1 ] @ twoToFive // @ concats two lists
-let `` 2 to 5 `` = [| 2; 3; 4; 5 |]   // Lines in between creates array
+let anArray = [| 2; 3; 4; 5 |]        // array
 
 // The "let" keyword also defines a named function.
 let square x = x * x  // Note that no parens are used.
-square 3              // Now run the function. Again, no parens.
+let nine = square 3   // Now run the function. Again, no parens.
 
 // can specify types and spread declaration over lines
 let add    // function name
@@ -24,7 +24,7 @@ let add    // function name
   = 
   x + y // function body 
 
-add 1 2
+let three = add 1 2
 
 // to define a multiline function, just use indents. No semicolons needed.
 let evens list =
